363a6f099e966aee560916209489f432860a1161
Kasia Izak <kontakt@kasiaizak.pl>
2019-10-25 23:19:39 +0200

    feat: dynamic color change for svg logo

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,13 @@
 const contentSwitcher = document.querySelector('.button__menu--js')
 
 contentSwitcher.addEventListener('click', (e) => {
+  const logo = document.querySelector('.navbar__item--logo-js');
   const header = document.querySelector('.header--js');
   const content = document.querySelector('.content--js');
 
+  logo.classList.toggle('navbar__item--logo-change-color');
   header.classList.toggle('header--hide');
+
 
   content.classList.toggle('content--visible');
   if (content.classList.contains('content--visible')) {
