59c61d69d511e255c2827efba437e26819664c9a
Kasia Izak <kontakt@kasiaizak.pl>
2019-10-25 23:31:56 +0200

    feat: change background color for content

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,14 @@
 const contentSwitcher = document.querySelector('.button__menu--js')
 
 contentSwitcher.addEventListener('click', (e) => {
+  const main = document.querySelector('.main--js');
   const logo = document.querySelector('.navbar__item--logo-js');
   const header = document.querySelector('.header--js');
   const content = document.querySelector('.content--js');
 
+  main.classList.toggle('main--change-color')
   logo.classList.toggle('navbar__item--logo-change-color');
   header.classList.toggle('header--hide');
-
 
   content.classList.toggle('content--visible');
   if (content.classList.contains('content--visible')) {
