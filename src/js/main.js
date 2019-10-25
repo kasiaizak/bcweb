fc9210babc79f71120509c330c67829c67da27b0
Kasia Izak <kontakt@kasiaizak.pl>
2019-10-25 19:37:37 +0200

    fix: change BEM and add hidden menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,18 @@
 "use strict";
 
-import moment from 'moment';
+// hamburger menu
+const contentSwitcher = document.querySelector('.button__menu--js')
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+contentSwitcher.addEventListener('click', (e) => {
+  const header = document.querySelector('.header--js');
+  const content = document.querySelector('.content--js');
+
+  header.classList.toggle('header--hide');
+
+  content.classList.toggle('content--visible');
+  if (content.classList.contains('content--visible')) {
+    contentSwitcher.innerHTML = '<i class="fas fa-times"></i>';
+  } else {
+    contentSwitcher.innerHTML = '<i class="fas fa-bars"></i>';
+  }
+});
